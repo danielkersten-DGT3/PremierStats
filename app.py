316807b51497b489fd23cff5d20f8828af9aa748
danielkersten-DGT3 <danielkersten@garincollege.nz>
2026-07-02 09:08:38 +1200

    4

--- app.py
+++ app.py
@@ -73,6 +73,136 @@ def create_app():
 
 
     db.init_app(app)
+    with app.app_context():
+        db.create_all()
+        if Clubs.query.count() == 0:
+
+            arsenal = Clubs(
+                ClubName="Arsenal",
+                Wins=20,
+                Losses=6
+            )
+
+            liverpool = Clubs(
+                ClubName="Liverpool",
+                Wins=25,
+                Losses=4
+            )
+
+            mancity = Clubs(
+                ClubName="Manchester City",
+                Wins=21,
+                Losses=7
+            )
+
+            chelsea = Clubs(
+                ClubName="Chelsea",
+                Wins=18,
+                Losses=8
+            )
+
+            manunited = Clubs(
+                ClubName="Manchester United",
+                Wins=16,
+                Losses=10
+            )
+
+            db.session.add_all([
+                arsenal,
+                liverpool,
+                mancity,
+                chelsea,
+                manunited
+            ])
+        if Managers.query.count() == 0:
+            m_arteta = Managers(Name="Mikel Arteta")
+            m_slot = Managers(Name="Arne Slot")
+            m_guardiola = Managers(Name="Pep Guardiola")
+            m_maresca = Managers(Name="Enzo Maresca")
+            m_amorim = Managers(Name="Ruben Amorim")
+
+            db.session.add_all([
+                m_arteta,
+                m_slot,
+                m_guardiola,
+                m_maresca,
+                m_amorim
+            ])
+        if Players.query.count() == 0:
+            p_saka = Players(
+                Name="Bukayo Saka",
+                Goals=12,
+                Assists=10,
+                Saves=0,
+                StartDate="2020-09-01",
+                EndDate=None
+            )
+
+            p_rice = Players(
+                Name="Declan Rice",
+                Goals=7,
+                Assists=8,
+                Saves=0,
+                StartDate="2023-07-15",
+                EndDate=None
+            )
+
+            p_haaland = Players(
+                Name="Erling Haaland",
+                Goals=27,
+                Assists=5,
+                Saves=0,
+                StartDate="2022-07-01",
+                EndDate=None
+            )
+
+            p_salah = Players(
+                Name="Mohamed Salah",
+                Goals=24,
+                Assists=16,
+                Saves=0,
+                StartDate="2017-07-01",
+                EndDate=None
+            )
+
+            p_palmer = Players(
+                Name="Cole Palmer",
+                Goals=16,
+                Assists=11,
+                Saves=0,
+                StartDate="2023-09-01",
+                EndDate=None
+            )
+
+            p_onana = Players(
+                Name="Andre Onana",
+                Goals=0,
+                Assists=0,
+                Saves=124,
+                StartDate="2023-07-20",
+                EndDate=None
+            )
+
+            p_alisson = Players(
+                Name="Alisson Becker",
+                Goals=0,
+                Assists=1,
+                Saves=132,
+                StartDate="2018-07-19",
+                EndDate=None
+            )
+
+            db.session.add_all([
+                p_saka,
+                p_rice,
+                p_haaland,
+                p_salah,
+                p_palmer,
+                p_onana,
+                p_alisson
+            ])
+
+            db.session.commit()
 
    
 
